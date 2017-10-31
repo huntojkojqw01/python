6fe7854da2b2123a834a88adf7bfbc4fd055b98f
hungnd <hungnd.k58@gmail.com>
2017-10-31 11:48:39 +0700

    ve duoc 2 do thi tren mot figure

--- tkinter_test.py
+++ tkinter_test.py
@@ -19,25 +19,43 @@ image = PhotoImage(file="./smilingpython.gif")
 label=Label(mainWindow,image=image,text="Nguyen Dinh Hung")
 label.grid(row=0,column=1)
 
-def show():
+fig, ax = plt.subplots(2, 1, num='Sound Diagram')
+fig.suptitle('Test wav file', fontsize=20)
+
+def show():	
+	global data,fs,filename,ax
 	print("Ban vua bam nut "+label['text'])
 	
-	filename =  filedialog.askopenfilename(initialdir = "~/Music",title = "Choose your file",filetypes = (("wav files","*.wav"),("mp3 files","*.mp3"),("all files","*.*")))
+	# filename =  filedialog.askopenfilename(initialdir = "~/Music",title = "Choose your file",filetypes = (("wav files","*.wav"),("mp3 files","*.mp3"),("all files","*.*")))
+	filename = "/home/hungnd/Music/khoosoothunhus.wav"
 	if not filename is None:
 		fs, data = wavfile.read(filename)
 		print(fs)
 		print(data)
-		X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
-		print(X)
-		C, S = np.cos(X), np.sin(X)
-
-		plt.plot(X, C)
-		plt.plot(X, S)
-
-		plt.show()
+		timeArray=np.array(list(range(1,len(data)+1)))/fs
+		print(len(timeArray),len(data))			
+		ax[0].plot(timeArray, data)
+		ax[0].set_xlabel('Time')
+		ax[0].set_ylabel('Amplitude')		
+		plt.show()		
+		
+def showfft():
+	global data,fs,filename,ax
+	print(filename,data,fs)
+	print(np.fft.fft(data))
+	X = np.linspace(-np.pi, np.pi, 256, endpoint=True)	
+	C, S = np.cos(X), np.sin(X)
+			
+	ax[1].plot(X, C)
+	ax[1].plot(X, S)	
+	ax[1].set_xlabel('Freq (Hz)')
+	ax[1].set_ylabel('|Y(freq)|')
+	plt.show()
 
 button=Button(mainWindow,text="Choose file",command=show)
 button.grid(row=1,column=1)
+button2=Button(mainWindow,text="FFT",command=showfft)
+button2.grid(row=2,column=1)
 
 mainWindow.geometry('280x350+500+200')
 mainWindow.title("いいね")
